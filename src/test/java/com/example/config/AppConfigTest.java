e639e4f49ca84af0f1cd77c5663c010d0eccdf4e
ued123 <32861341+ued123@users.noreply.github.com>
2022-05-31 20:42:17 +0900

    ### 몽고디비 연동 > - 유닛테스트 코드 수정 Assertion 사용 > - 어플리케이션 시작방법 README 추가

--- src/test/java/com/example/config/AppConfigTest.java
+++ src/test/java/com/example/config/AppConfigTest.java
@@ -3,24 +3,29 @@ package com.example.config;
 import com.mongodb.client.ClientSession;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class AppConfigTest {
 
     ApplicationContext ac;
-
     MongoTemplate mongoTemplate;
-
     MongoClient mongoClient;
 
     @BeforeEach
     void init(){
         ac = new AnnotationConfigApplicationContext(AppConfig.class);
-
         mongoClient = ac.getBean("mongoClient",MongoClient.class);
     }
 
@@ -28,9 +33,9 @@ class AppConfigTest {
     public void mongodbConntest() {
 
         MongoDatabase mongoDatabase = mongoClient.getDatabase("vending-machine");
+        Bson command = new BsonDocument("ping", new BsonInt64(1));
+        Document commandResult = mongoDatabase.runCommand(command);
 
-        ClientSession session = mongoClient.startSession();
-
-        System.out.println(session);
+        assertNotNull(commandResult);
     }
 }
