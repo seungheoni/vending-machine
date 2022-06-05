5a4b0f5e2f0e47354fdca9afba06b2a85cdeec16
ued123 <32861341+ued123@users.noreply.github.com>
2022-06-05 13:25:33 +0900

    mongodb 연동 & container 환경 구성 (#5)
    * ### 몽고디비 연동
    > - mongodb docker-compose.yml 작성
    > - 더미데이터 init data json 작성
    > - mongoTemplate, mongoClient 수동 빈 등록
    > - test코드 작성. 현재는 커넥션 연결 상태만 확인. startSession
    
    * ### 몽고디비 연동
    > - 데이터베이스 이름 수정
    >   - ~~vending-machine~~ vending-machine 수정
    
    * ### 몽고디비 연동
    > - 유닛테스트 코드 수정 Assertion 사용
    > - 어플리케이션 시작방법 README 추가
    
    * ### 몽고디비 연동
    > - EnableMongoRepositories를 통한 MongoRepository interface 로 자동 구현 (AppConfig.java)
    
    ### mongodb document 정의
    > - ObjectId @MongoId를 통한 id 정의
    
    ### 테스트 컨테이너 환경 구축 resolve #4 
    > - testcontainer 라이브러리 추가하여 junit 환경에서 컨테이너 테스트 환경 구축
    > - mongodb 테스트 컨테이너 구축
    > - application-test.yml 추가하여 테스트 설정 파일 추가
    
    ### mongodb repository connect & crud 테스트
    > - mongodb connect test (DbIntegrationTests.java)
    > - insert,save,delete,find 메서드를 통한 crud junit 통합 테스트 (DrinkRepositoryTests.java)

--- src/main/java/com.example/drink/service/DrinkServiceImpl.java
+++ src/main/java/com.example/drink/service/DrinkServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.drink.service;
 
 import com.example.drink.dto.DrinkDto;
 import com.example.drink.dto.OrderDto;
+import com.example.drink.model.Drink;
 import com.example.drink.repo.DrinkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,25 +13,14 @@ public class DrinkServiceImpl implements DrinkService {
 
     private final DrinkRepository drinkRepository;
 
-    @Override
-    public DrinkDto sellDrink(OrderDto order) throws Exception {
 
-         /*
-            음료 판매
-            1. 음료가 존재하지 않으면 에러메시지
-            2. 고객의 금액이 부족한 경우 에러 메시지
-            3. 구매
-         */
-        DrinkDto drink = getDrink(order.getDrinkName());
-
-        // 보유 금액
-        int amount = order.getDrinkPrice();
-        if(!drink.enablePurchase(amount)) throw new Exception("잔액이 부족합니다.");
-        return drink;
+    private Drink getDrink(String drinkName) throws Exception {
+        return drinkRepository.findByName(drinkName)
+                .orElseThrow(() -> new Exception("존재하지 않는 음료수 입니다."));
     }
 
-    private DrinkDto getDrink(String drinkName) throws Exception {
-        return drinkRepository.findByDrinkName(drinkName)
-                .orElseThrow(() -> new Exception("존재하지 않는 음료수 입니다."));
+    @Override
+    public DrinkDto sellDrink(OrderDto order) throws Exception {
+        return null;
     }
 }
