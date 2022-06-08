0125c986776b15bb6098ed5f4853755ab38522ff
ued123 <32861341+ued123@users.noreply.github.com>
2022-06-08 09:43:28 +0900

    ### gradle을 통한 모듈 분리 > - web,api,mongodb 로 도메인 별 모듈 분리

---
+++ web/src/main/java/com/example/drink/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.drink.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainframe(Model model) {
+
+        model.addAttribute("name","음료수 자판기");
+
+        return "mainframe";
+    }
+}
