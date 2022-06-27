0d686a8be1a1e5e0c9004d649985be27461728db
ued123 <32861341+ued123@users.noreply.github.com>
2022-06-27 21:04:40 +0900

    ## pr 수정 #18 ### drink-api > - MongoInitDataRunner.java 개발모드 초기화 로직 수정 >   - deleteAll을 활용하여 clear이후 데이터 초기화 >   - ~~@transaction~~ 제거 , 없어도 정상 실행 확인

--- drink-api/src/main/java/com/example/mongo/config/MongoInitDataRunner.java
+++ drink-api/src/main/java/com/example/mongo/config/MongoInitDataRunner.java
@@ -24,21 +24,24 @@ public class MongoInitDataRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
+        System.out.println("개발모드 활성화...");
+        clearData();
         initData();
     }
 
-    @Transactional
     public void initData() {
 
-        System.out.println("몽고 디비 데이터 초기화 중... ");
-        drinkRepository.deleteAll();
-
-        System.out.println("몽고 디비 초기화 데이터 생성중... ");
+        System.out.println("컬렉션 초기화 데이터 생성중... ");
         List<Drink> drinks = Arrays.asList(new Drink(ObjectId.get(),"콜라",2000,30, List.of(new Display(1),new Display(2),new Display(3))),
                 new Drink(ObjectId.get(),"식헤",3000,30, List.of(new Display(4),new Display(5),new Display(6))),
                 new Drink(ObjectId.get(),"사이다",1500,0, List.of(new Display(7),new Display(8))));
 
         drinkRepository.saveAll(drinks);
-        System.out.println("몽고 디비 초기화 데이터 생성 완료... ");
+        System.out.println("컬렉션 초기화 데이터 생성 완료... ");
+    }
+
+    public void clearData() {
+        System.out.println("컬렉션 데이터 비우는 중... ");
+        drinkRepository.deleteAll();
     }
 }
