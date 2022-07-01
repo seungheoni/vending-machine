0f64968e7d43089ce26db9af81e37dcab0ddc6bf
hoya <hybriddevslim@gmail.com>
2022-07-01 21:38:04 +0900

    음료 전시 API 추가

--- drink-api/src/main/java/com/example/display/repo/DisplayRepository.java
+++ drink-api/src/main/java/com/example/display/repo/DisplayRepository.java
@@ -2,8 +2,23 @@ package com.example.display.repo;
 
 import com.example.mongo.model.Display;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface DisplayRepository extends MongoRepository<Display, ObjectId> {
+
+    @Aggregation(pipeline = {
+            "{ $lookup:\n" +
+            "       {\n" +
+            "         from: drink,\n" +
+            "         localField: drinkId,\n" +
+            "         foreignField: _id,\n" +
+            "         as: drinks\n" +
+            "       }" +
+            "}"})
+    List<Display> findWithDrink();
 }
