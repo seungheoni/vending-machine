894a5ed1e47e1094148a27f51f59eaa1e13d9565
hoya <hybriddevslim@gmail.com>
2022-07-23 14:26:53 +0900

    DisplayControllerTest 수정 - objectMapper 추가 - 응답값 검사 방식 수정

--- drink-api/src/test/java/com/example/display/controller/DisplayControllerTest.java
+++ drink-api/src/test/java/com/example/display/controller/DisplayControllerTest.java
@@ -1,7 +1,11 @@
 package com.example.display.controller;
 
+import com.example.display.dto.DisplayDrinkView;
 import com.example.display.fixture.DisplayFixture;
 import com.example.display.service.DisplayService;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +15,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -25,18 +32,28 @@ public class DisplayControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private DisplayService displayService;
-
 
     @Test
     public void drinksService() throws Exception {
 
         when(displayService.getDrinkAllList())
-                .thenReturn(List.of(DisplayFixture.of(1, "콜라"),DisplayFixture.of(2, "식혜"),DisplayFixture.of(3, "사이다")));
+                .thenReturn(List.of(DisplayFixture.of(1, "콜라"), DisplayFixture.of(2, "식혜"), DisplayFixture.of(3, "사이다")));
 
         mvc.perform(get("/drinks/display") //해당 url로 요청을 한다.
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED) // Json 타입으로 지정
-        ).andExpect(status().isOk()).andDo(print());
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED) // Json 타입으로 지정
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(result -> {
+                    byte[] content = result.getResponse().getContentAsByteArray();
+                    List<DisplayDrinkView> results = objectMapper.readValue(content, new TypeReference<>() {
+                    });
+                    assertThat(results.get(0), isA(DisplayDrinkView.class));
+                });
     }
 }
