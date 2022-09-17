40246c5572666f1153d39fca628a0181d59d04eb
ued123 <32861341+ued123@users.noreply.github.com>
2022-09-17 13:15:30 +0900

    * Mongo Auditing 추가 > - @EnableMongoAuditing 추가
    * cash 도메인 추가
    > - @CreatedDate,@LastModifiedDate 추가

---
+++ cash-api/src/main/java/com/example/mongo/model/Cash.java
@@ -0,0 +1,40 @@
+package com.example.mongo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.Instant;
+
+@Document
+@Data
+@AllArgsConstructor
+public class Cash {
+
+    /**
+     * cash id
+     */
+    @MongoId
+    private ObjectId id;
+
+    /**
+     * 금액 잔고
+     */
+    private Long balance;
+
+    /**
+     * 데이터 생성 날짜
+     */
+    @CreatedDate
+    private Instant createDate;
+
+    /**
+     * 데이터 업데이트 날짜
+     */
+    @LastModifiedDate
+    private Instant updateDate;
+}
