5534c0fea60c567a076bc3fe8b82e7fe65b3871b
hoya <hybriddevslim@gmail.com>
2022-10-05 00:42:35 +0900

    Testcontainers 수정 - 상속을 통하여 테스트 케이스 작성

--- cash-api/src/test/groovy/com/example/transaction/repo/TransactionRepositoryTest.groovy
+++ cash-api/src/test/groovy/com/example/transaction/repo/TransactionRepositoryTest.groovy
@@ -1,52 +1,31 @@
 package com.example.transaction.repo
 
-import com.example.mongo.config.MongoConfigurer
 import com.example.mongo.model.Transaction
 import com.example.mongo.model.TransactionType
+import mongo.DbIntegrationTest
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.containers.MongoDBContainer
-import org.testcontainers.spock.Testcontainers
-import org.testcontainers.utility.DockerImageName
-import spock.lang.Shared
-import spock.lang.Specification
 
-@Testcontainers
-@ActiveProfiles("test")
-@DataMongoTest
-@ContextConfiguration(classes = [MongoConfigurer.class])
-class TransactionRepositoryTest extends Specification {
+class 가 extends DbIntegrationTest {
 
-    @Shared
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"))
-
-    static {
-        mongoDBContainer.start()
-    }
-
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", () -> mongoDBContainer.getReplicaSetUrl())
-    }
     @Autowired
     TransactionRepository transactionRepository
 
     def "거래 내역 데이터 조회"() {
         given: "500원의 입금 내역 데이터를 생성"
+        transactionRepository.deleteAll()
         Transaction expected = Transaction.ofDeposit(500)
-        when: "입금 내역을 db 에 저장"
+        when: "입금 내역을 db 에 저장할 떄"
         Transaction result = transactionRepository.save(expected)
-        then: "결과값의 type deposit 이고, 금액은 500원이다"
+        then: "결과값의 type deposit 이고, 금액은 기대값과 일치한다"
         verifyAll {
             result.getAmount() == expected.getAmount()
             result.getType() == TransactionType.DEPOSIT
         }
-
-
+        when: "거래 내역 모두 제거할 때"
+        transactionRepository.deleteAll()
+        then: "조회시 거래 내역은 존재하지 않는다."
+        verifyAll {
+            transactionRepository.count() == 0
+        }
     }
-
 }
