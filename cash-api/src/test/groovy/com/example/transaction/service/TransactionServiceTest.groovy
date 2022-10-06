5c977959555cc64971d7f5816fb051a901b4160e
caramelsyrup <seojunlim87@gmail.com>
2022-10-07 00:10:41 +0900

    TransactionServiceTest.groovy 생성

---
+++ cash-api/src/test/groovy/com/example/transaction/service/TransactionServiceTest.groovy
@@ -0,0 +1,29 @@
+package com.example.transaction.service
+
+import com.example.mongo.model.Transaction
+import com.example.mongo.model.TransactionType
+import com.example.transaction.fixture.TransactionBuilder
+import com.example.transaction.repo.TransactionRepository
+import spock.lang.Specification
+
+class TransactionServiceTest extends Specification {
+
+    def transactionRepository = Stub(TransactionRepository.class)
+    TransactionServiceImpl transactionServiceImpl = new TransactionServiceImpl(transactionRepository)
+
+    def "입금 거래 내역 생성"() {
+        given: "5000원의 입금 내역 데이터를 생성"
+        Transaction expected = TransactionBuilder.make {
+            type TransactionType.DEPOSIT
+            amount 4000
+        }.build()
+        when: "transactionRepository.save() 수행한 경우"
+        transactionRepository.save({ expected } as Transaction) >> expected
+        then: "transactionServiceImpl.deposit()의 결과값의 type 과 amount는 기대값과 같다"
+        def result = transactionServiceImpl.deposit(5000)
+        verifyAll {
+            result.type == expected.type
+            result.amount == expected.amount
+        }
+    }
+}
