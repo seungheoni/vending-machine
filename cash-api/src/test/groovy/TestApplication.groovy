2261e8f96a79fd205a39f5ee4157898750f3677c
caramelsyrup <seojunlim87@gmail.com>
2022-10-07 23:22:53 +0900

    groovy 폴더 제거

--- cash-api/src/test/groovy/TestApplication.groovy
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication
-class TestApplication {
-    static void main(String[] args) {
-        SpringApplication.run(com.example.TestApplication.class, args)
-    }
-}
