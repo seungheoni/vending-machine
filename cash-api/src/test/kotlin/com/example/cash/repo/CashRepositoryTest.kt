9788e0582042e60d97dadfcc65ec0cee818e1de3
ued123 <32861341+ued123@users.noreply.github.com>
2022-10-12 21:19:43 +0900

    cashRepository 테스트 코틀린 kotest 활용한 bdd 스타일로 변경

---
+++ cash-api/src/test/kotlin/com/example/cash/repo/CashRepositoryTest.kt
@@ -0,0 +1,45 @@
+package com.example.cash.repo
+
+import com.example.mongo.model.Cash
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+
+@DataMongoTest
+class CashRepositoryTest(
+    private val cashRepository : CashRepository
+) : BehaviorSpec({
+
+    Given("입금 상태가 0원의 상태에서") {
+        cashRepository.deleteAll()
+        When("1000원을 입금하면") {
+            val result = cashRepository.save(Cash.of(1000L))
+            Then("1000원의 잔액데이터를 보여줘야한다.") {
+                result.balance shouldBe 1000L
+            }
+        }
+    }
+
+    Given("잔액 데이터 1개 존재한 상태에서") {
+        cashRepository.deleteAll()
+        val result = cashRepository.save(Cash.of(1000L))
+        When("잔액 전체 데이터를 조회 하면") {
+            val result = cashRepository.findAll()
+            Then("거래 내역 리스트는 1개를 보여준다.") {
+                result.size shouldBe 1;
+            }
+        }
+    }
+
+    Given("조회시 잔액 데이터 1개 존재하고") {
+        cashRepository.deleteAll()
+        val cash = cashRepository.save(Cash.of(1000L))
+        When("조회된 잔액 데이터를 삭제 하고 전체 조회를 하면"){
+            cashRepository.delete(cash);
+            val result = cashRepository.findAll();
+            Then("결과 데이터는 0이 나온다.") {
+                result.size shouldBe 0
+            }
+        }
+    }
+})
