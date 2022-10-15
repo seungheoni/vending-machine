9a3ba74a412be85c662524bcb3edd75c27269dea
ued123 <32861341+ued123@users.noreply.github.com>
2022-10-15 12:59:39 +0900

    cashController 유닛 테스트 bdd 스타일로 변경 - ~~StringSpec~~ -> BehaviorSpec 으로 변경

--- cash-api/src/test/kotlin/com/example/cash/controller/CashControllerTest.kt
+++ cash-api/src/test/kotlin/com/example/cash/controller/CashControllerTest.kt
@@ -4,9 +4,8 @@ import com.example.cash.dto.CashDepositPayLoad
 import com.example.cash.dto.CashDepositView
 import com.example.cash.service.CashService
 import com.ninjasquad.springmockk.MockkBean
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.every
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
@@ -19,20 +18,26 @@ import org.springframework.web.reactive.function.BodyInserters
 class CashControllerUnitTest(
     @MockkBean private val cashService: CashService,
     private val webTestClient: WebTestClient
-) : StringSpec({
+) : BehaviorSpec({
 
-    "/cash/deposit 테스트" {
-        val cash = CashDepositView(1000L)
+    Given("/cash/deposit 테스트") {
+        When("cashService 0원인 상태에서 1000원을 넣으면 1000원 잔액 데이터를 보여준다") {
+            val cash = CashDepositView(1000L)
+            every { cashService.deposit(CashDepositPayLoad(1000L)) } returns CashDepositView(1000L)
 
-        every { cashService.deposit(CashDepositPayLoad(1000L)) } returns CashDepositView(1000L)
+            Then("0원인 상태에서 payload 규격에 맞춘 1000원을 요청하면 잔액 1000원을 보여준다.") {
 
-        webTestClient
-            .put()
-            .uri("/cash/deposit")
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(BodyInserters.fromValue(CashDepositPayLoad(1000L)))
-            .exchange()
-            .expectStatus().isOk
-            .expectBody<CashDepositView>().isEqualTo(cash)
+                webTestClient
+                    .put()
+                    .uri("/cash/deposit")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(BodyInserters.fromValue(CashDepositPayLoad(1000L)))
+                    .exchange()
+                    .expectStatus().isOk
+                    .expectBody<CashDepositView>()
+                    .isEqualTo(cash)
+
+            }
+        }
     }
 })
