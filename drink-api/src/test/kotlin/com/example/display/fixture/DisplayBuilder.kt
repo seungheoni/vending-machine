4d6f70d665b2fdf95e49c51314ae759060c4eadf
ued123 <32861341+ued123@users.noreply.github.com>
2022-10-18 08:45:57 +0900

    중간과정 커밋

---
+++ drink-api/src/test/kotlin/com/example/display/fixture/DisplayBuilder.kt
@@ -0,0 +1,21 @@
+package com.example.display.fixture
+
+import com.example.mongo.model.Display
+import com.example.mongo.model.Drink
+import org.bson.types.ObjectId
+
+
+inline fun display(block: DisplayBuilder.() -> Unit = {}) =
+    DisplayBuilder().apply(block).build()
+
+class DisplayBuilder {
+
+    var id: ObjectId? = null
+    var position: Int = 0
+    var drinkId: ObjectId? = null
+    var drinks: List<Drink>? = null
+
+    fun build(): Display = Display(
+        id,position,drinkId,drinks
+    )
+}
---
+++ drink-api/src/test/kotlin/com/example/display/repo/DisplayRepositoryTest.kt
@@ -0,0 +1,34 @@
+package com.example.display.repo
+
+import com.example.mongo.model.Display
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import org.bson.types.ObjectId
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+
+@DataMongoTest
+class DisplayRepositoryTest(
+    private val displayRepository: DisplayRepository
+):BehaviorSpec ({
+
+    Given("display 데이터 1개를 생성하고") {
+        val display = Display.of(0, ObjectId.get())
+        When("display 데이터 1개를 저장하면") {
+            val result = displayRepository.save(display);
+            Then("1개의 Display를 성공적으로 반환한다.") {
+                result is Display
+            }
+        }
+    }
+
+    Given("display 데이터를 생성하고"){
+        val display = Display.of(0, ObjectId.get())
+        displayRepository.save(display)
+        When("display 데이터 1개를 삭제하면") {
+            val result = displayRepository.delete(display);
+            Then("성공시 void를 반환한다.") {
+                result shouldBe kotlin.Unit
+            }
+        }
+    }
+})
