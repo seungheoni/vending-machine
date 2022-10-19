17014b16e56bfabe2472ba0c96b2e24ab070fea2
ued123 <32861341+ued123@users.noreply.github.com>
2022-10-19 21:21:12 +0900

    DisplayService kotest 검증 로직 수정 - ~~shouldBeInstanceOf~~ -> shouldBeSameInstanceAs

--- drink-api/src/test/kotlin/com/example/display/service/DisplayServiceTest.kt
+++ drink-api/src/test/kotlin/com/example/display/service/DisplayServiceTest.kt
@@ -2,9 +2,8 @@ package com.example.display.service
 
 import com.example.display.repo.DisplayRepository
 import com.example.mongo.model.Display
-import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.types.shouldBeInstanceOf
+import io.kotest.matchers.types.shouldBeSameInstanceAs
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
@@ -21,15 +20,13 @@ class DisplayServiceTest: BehaviorSpec ({
 
     Given("display와 drink의 룩업된 전체 리스트 조회 서비스") {
         When("displayServiceImpl의 displayDrinks 조회시") {
+            val expected = listOf(Display.of(1,ObjectId.get()))
             every {
                 displayRepository.findWithDrink();
-            } returns listOf(Display.of(1,ObjectId.get()))
+            } returns expected
             val result = displayServiceImpl.displayDrinks
             Then("drink 컬렉션과 룩업된 Display 객체 리스트를 반환한다.") {
-                assertSoftly {
-                    result.shouldBeInstanceOf<List<Display>>()
-                    result[0].shouldBeInstanceOf<Display>()
-                }
+                result.shouldBeSameInstanceAs(expected)
             }
         }
     }
