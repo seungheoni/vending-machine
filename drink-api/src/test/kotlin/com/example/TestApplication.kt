e86f5383e0480d8afc74e395decfa12b55a58700
ued123 <32861341+ued123@users.noreply.github.com>
2022-10-20 21:09:35 +0900

    기존 java junit 테스트 파일 삭제, 코틀린 웹 테스트용 TestApplication.kt 추가

---
+++ drink-api/src/test/kotlin/com/example/TestApplication.kt
@@ -0,0 +1,14 @@
+package com.example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class TestApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestApplication>(*args)
+}
+
+
