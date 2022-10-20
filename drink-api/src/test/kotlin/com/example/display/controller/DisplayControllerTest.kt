dfdd143b18f6fa8429c63e3e6e3233608d5b4509
이승헌 <32861341+ued123@users.noreply.github.com>
2022-10-20 20:18:11 +0900

    Display 도메인 유닛테스트 bdd 스타일로 변경 (#89)
    - repo,service,controller kotest BehaviorSpec 적용

---
+++ drink-api/src/test/kotlin/com/example/display/controller/DisplayControllerTest.kt
@@ -0,0 +1,47 @@
+package com.example.display.controller
+
+import com.example.display.dto.DisplayDrinkView
+import com.example.display.fixture.display
+import com.example.display.service.DisplayService
+import com.example.drink.enums.Status
+import com.example.mongo.model.Display
+import com.example.mongo.model.Drink
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import org.bson.types.ObjectId
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+import reactor.core.publisher.Mono
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class DisplayControllerTest(
+    @MockkBean private val displayService : DisplayService,
+    private val webTestClient: WebTestClient
+) : BehaviorSpec({
+
+    Given("/drinks/display 테스트") {
+        When("cashService 0원인 상태"){
+            Then("요청하면 displaydrinkview 형태의 음료수 전시 데이터를 리턴한다."){
+                every { displayService.displayDrinks } returns listOf((display {
+                    id = ObjectId.get()
+                    position = 1
+                    drinkId = ObjectId.get()
+                    drinks = listOf<Drink>(Drink.of("콜라", 1000,5));
+                }))
+
+                webTestClient
+                    .get()
+                    .uri("/drinks/display")
+                    .exchange()
+                    .expectStatus().isOk
+                    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                    .expectBody<List<DisplayDrinkView>>()
+            }
+        }
+    }
+})
