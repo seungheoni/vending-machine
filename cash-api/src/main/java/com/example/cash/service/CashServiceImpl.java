d73307ef4f8e854bcc65d13644bf53845d16a469
ued123 <32861341+ued123@users.noreply.github.com>
2022-10-23 16:37:52 +0900

    cashServiceImpl deposit 구현 로직 추가

---
+++ cash-api/src/main/java/com/example/cash/service/CashServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.cash.service;
+
+import com.example.cash.dto.CashDepositView;
+import com.example.cash.repo.CashRepository;
+import com.example.mongo.model.Cash;
+import com.example.transaction.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CashServiceImpl implements CashService {
+
+    private final CashRepository cashRepository;
+
+    private final TransactionService transactionService;
+
+    @Override
+    public CashDepositView deposit(final Long amount) {
+
+        transactionService.deposit(amount);
+        Cash cash = cashRepository.findAll().get(0);
+        cash.setBalance(cash.getBalance() + amount);
+        cashRepository.save(cash);
+        return CashDepositView.of(cash.getBalance());
+    }
+}
---
+++ cash-api/src/test/kotlin/com/example/cash/service/CashServiceTest.kt
@@ -0,0 +1,48 @@
+package com.example.cash.service
+
+import com.example.cash.dto.CashDepositView
+import com.example.cash.repo.CashRepository
+import com.example.mongo.model.Cash
+import com.example.mongo.model.Transaction
+import com.example.transaction.service.TransactionServiceImpl
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
+
+class CashServiceTest : BehaviorSpec({
+
+    val transactionServiceImpl : TransactionServiceImpl = mockk();
+    val cashRepository :  CashRepository = mockk()
+
+    val cashService = CashServiceImpl(cashRepository,transactionServiceImpl)
+
+    beforeEach {
+        clearAllMocks()
+    }
+
+    Given("cashService deposit 테스트") {
+        When("자판기의 잔액이 1000원인 상태에서 1000원을 추가로 넣으면") {
+            val amount = 1000L
+            val expected = Cash.of(amount)
+            every {
+                cashRepository.save(expected)
+            } returns expected
+
+            every {
+                cashRepository.findAll()
+            } returns listOf(expected)
+
+            every {
+                transactionServiceImpl.deposit(amount)
+            } returns Transaction.ofDeposit(amount)
+
+            val cashDepositView : CashDepositView = cashService.deposit(amount)
+            Then("2000원의 잔액 결과를 반환한다. (또한 1000원에 해당하는 거래 내역을 생성한다.)") {
+                cashDepositView.balance shouldBe 2000L
+            }
+        }
+    }
+
+})
