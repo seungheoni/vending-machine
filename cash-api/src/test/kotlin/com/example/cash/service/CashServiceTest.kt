ed6981dc32c6ef15cfd2d99e0c0ccbbed7bc279d
hoya <hybriddevslim@gmail.com>
2022-10-29 21:09:08 +0900

    CashRepositoryTest, CashServiceTest: 테스트 코드 일부 수정

--- cash-api/src/test/kotlin/com/example/cash/service/CashServiceTest.kt
+++ cash-api/src/test/kotlin/com/example/cash/service/CashServiceTest.kt
@@ -1,6 +1,5 @@
 package com.example.cash.service
 
-import com.example.cash.dto.CashDepositView
 import com.example.cash.repo.CashRepository
 import com.example.mongo.model.Cash
 import com.example.mongo.model.Transaction
@@ -10,10 +9,11 @@ import io.kotest.matchers.shouldBe
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
+import java.util.*
 
 class CashServiceTest : BehaviorSpec({
 
-    val transactionServiceImpl : TransactionServiceImpl = mockk();
+    val transactionServiceImpl : TransactionServiceImpl = mockk()
     val cashRepository :  CashRepository = mockk()
 
     val cashService = CashServiceImpl(cashRepository,transactionServiceImpl)
@@ -25,22 +25,25 @@ class CashServiceTest : BehaviorSpec({
     Given("cashService deposit 테스트") {
         When("자판기의 잔액이 1000원인 상태에서 1000원을 추가로 넣으면") {
             val amount = 1000L
-            val expected = Cash.of(amount)
-            every {
-                cashRepository.save(expected)
-            } returns expected
+            val cash = Cash.of(amount)
+            val depositCash = cash.deposit(amount)
+            val expected = depositCash.toCashDepositView()
 
             every {
                 cashRepository.findFirstBy()
-            } returns expected
+            } returns Optional.of(cash)
 
             every {
                 transactionServiceImpl.deposit(amount)
             } returns Transaction.ofDeposit(amount)
 
-            val cashDepositView : CashDepositView = cashService.deposit(amount)
+            every {
+                cashRepository.save(depositCash)
+            } returns depositCash
+
+            val cashDepositView = cashService.deposit(amount)
             Then("2000원의 잔액 결과를 반환한다. (또한 1000원에 해당하는 거래 내역을 생성한다.)") {
-                cashDepositView.balance shouldBe 2000L
+                cashDepositView.balance shouldBe expected.balance
             }
         }
     }
