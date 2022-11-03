d3df9cae6b824494959153884cd1ed03539eecbb
ued123 <32861341+ued123@users.noreply.github.com>
2022-11-03 21:32:37 +0900

    ExceptionTranslator: 에러 정의되지 않은 Exception 발생시 처리 핸들러 추가

--- cash-api/src/main/java/com/example/ExceptionTranslator.java
+++ cash-api/src/main/java/com/example/ExceptionTranslator.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.example.error.ErrorBody;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,5 +17,16 @@ public class ExceptionTranslator {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorBody> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
         return ResponseEntity.badRequest().body(ErrorBody.of(exception));
+    }
+
+    /**
+     * 기타 Exception 예외 발생시 ResponseEntity 생성 로직
+     * @param exception @ModelAttribute 나 @RequestBody 유효성 검사 실패시 발생하는 예외
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorBody> etcException(Exception exception){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorBody.of(exception));
     }
 }
