55ea1eb8959facabb5817537c2ffbd4235a0f685
ued123 <32861341+ued123@users.noreply.github.com>
2022-11-03 21:34:10 +0900

    ErrorBody: 500 server error 용 필드 추가 & 생성 of 메서드 추가

--- cash-api/src/main/java/com/example/error/ErrorBody.java
+++ cash-api/src/main/java/com/example/error/ErrorBody.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.time.Instant;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 public class ErrorBody {
 
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private String message;
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
@@ -27,12 +29,32 @@ public class ErrorBody {
     private long timestamp;
 
     /**
-     * 에러 바디 객체 생성 함수
+     * ETC ERROR
+     */
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private int status;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String error;
+
+
+    /**
+     * 에러 바디 객체 생성 함수 (MethodArgumentNotValidException)
      * @param exception @ModelAttribute 나 @RequestBody 유효성 검사 실패시 발생하는 예외
      */
     public static ErrorBody of(MethodArgumentNotValidException exception) {
         List<SimpleFieldError> fields = exception.getBindingResult().getFieldErrors().stream().map(SimpleFieldError::of).collect(Collectors.toList());
         long timeStamp = Instant.now().toEpochMilli();
-        return new ErrorBody(ErrorMessage.INVALID_VALIDATION, null, fields, timeStamp);
+        return new ErrorBody(ErrorMessage.INVALID_VALIDATION, null, fields, timeStamp,0,null);
+    }
+
+
+    /**
+     * 에러 바디 객체 생성 함수 (EXCEPTION)
+     * @param exception @ModelAttribute 나 @RequestBody 유효성 검사 실패시 발생하는 예외
+     */
+    public static ErrorBody of(Exception exception) {
+        long timeStamp = Instant.now().toEpochMilli();
+        return new ErrorBody(null,null,null,timeStamp,HttpStatus.INTERNAL_SERVER_ERROR.value(),HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
     }
 }
