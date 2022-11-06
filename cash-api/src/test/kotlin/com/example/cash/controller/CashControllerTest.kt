d7d7ec90da04cfb3164aa42d218ebdf24b7fc5c5
ued123 <32861341+ued123@users.noreply.github.com>
2022-11-06 21:28:02 +0900

    CashControllerTest: 거스름돈 반환 api controller 테스트,200,404,500 케이스 생성

--- cash-api/src/test/kotlin/com/example/cash/controller/CashControllerTest.kt
+++ cash-api/src/test/kotlin/com/example/cash/controller/CashControllerTest.kt
@@ -1,11 +1,13 @@
 package com.example.cash.controller
 
+import com.example.cash.dto.CashChangeView
 import com.example.cash.dto.CashDepositPayLoad
 import com.example.cash.dto.CashDepositView
 import com.example.cash.service.CashService
 import com.example.error.ErrorBody
 import com.example.error.ErrorMessage
 import com.example.error.SimpleFieldError
+import com.example.error.exception.CashEmptyException
 import com.ninjasquad.springmockk.MockkBean
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.should
@@ -31,6 +33,11 @@ class CashControllerUnitTest(
             .uri("/cash/deposit")
             .contentType(MediaType.APPLICATION_JSON)
             .body(BodyInserters.fromValue(payload))
+
+    fun cashChangeClient() = webTestClient
+        .put()
+        .uri("/cash/change")
+        .contentType(MediaType.APPLICATION_JSON)
 
     Given("/cash/deposit") {
         When("payload 의 필드 값이 유효하지 않은 경우") {
@@ -83,4 +90,46 @@ class CashControllerUnitTest(
             }
         }
     }
+
+    Given("/cash/change") {
+        val amount = 1000L
+        When("자판기 잔액이 "+amount+"원인 상태에서 정상적으로 호출 했을때) "){
+            every { cashService.change() } returns CashChangeView(amount)
+            val exchange = cashChangeClient().exchange()
+            Then("status: 200 OK") {
+                exchange.expectStatus().isOk
+            }
+            Then("body: amount는 " + amount + "원이 된다.") {
+                exchange.expectBody<CashChangeView>().returnResult()
+                    .responseBody!!.amount shouldBe amount
+            }
+        }
+
+        When("자판기 잔액이 0원인 상태에서 호출 했을때") {
+            every { cashService.change() } throws CashEmptyException(HttpStatus.NOT_FOUND)
+            val exchange = cashChangeClient().exchange()
+            val expected = ErrorMessage.CASH_EMPTY
+            Then("status: 404 NOT FOUND") {
+                exchange.expectStatus().isNotFound
+            }
+
+            Then("body: message: " + expected + "이다") {
+                exchange.expectBody<ErrorBody>().returnResult().responseBody!!.message shouldBe expected
+            }
+        }
+
+        When("자판기 잔액이 "+amount+"원인 상태에서 500 server error 발생시") {
+            every { cashService.change() } throws Exception()
+            val exchange = cashChangeClient().exchange()
+            val expected = HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase
+            Then("status: 500 Internal Server Error") {
+                exchange.expectStatus().is5xxServerError
+            }
+
+            Then("body: message " + expected + "이다") {
+                exchange.expectBody<ErrorBody>().returnResult().responseBody!!.message shouldBe expected
+            }
+        }
+    }
+
 })
