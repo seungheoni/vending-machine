7e51c41bf5d9b98c6bdb6cad2df92e9d6a190b39
ued123 <32861341+ued123@users.noreply.github.com>
2022-11-06 21:23:29 +0900

    ErrorMessage: cash가 0원일때 발생하는 커스텀 에러 메시지 생성

--- cash-api/src/main/java/com/example/error/ErrorMessage.java
+++ cash-api/src/main/java/com/example/error/ErrorMessage.java
@@ -2,4 +2,6 @@ package com.example.error;
 
 public class ErrorMessage {
     public static final String INVALID_VALIDATION = "올바르지 않은 형식입니다";
+
+    public static final String CASH_EMPTY = "잔액이 없습니다.";
 }
