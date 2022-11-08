35a8be9c048f6ec5a18459d566d8bb2892fa3f62
ued123 <32861341+ued123@users.noreply.github.com>
2022-11-08 21:56:57 +0900

    CashEmptyException: 부모 클래스 생성자 호출을 통한 에러 바디 정의

--- cash-api/src/main/java/com/example/error/exception/CashEmptyException.java
+++ cash-api/src/main/java/com/example/error/exception/CashEmptyException.java
@@ -1,23 +1,12 @@
 package com.example.error.exception;
 
+import com.example.error.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public class CashEmptyException extends ResponseStatusException {
 
     public CashEmptyException(HttpStatus status) {
-        super(status);
-    }
-
-    public CashEmptyException(HttpStatus status, String reason) {
-        super(status, reason);
-    }
-
-    public CashEmptyException(HttpStatus status, String reason, Throwable cause) {
-        super(status, reason, cause);
-    }
-
-    public CashEmptyException(int rawStatusCode, String reason, Throwable cause) {
-        super(rawStatusCode, reason, cause);
+        super(status, ErrorMessage.CASH_EMPTY);
     }
 }
