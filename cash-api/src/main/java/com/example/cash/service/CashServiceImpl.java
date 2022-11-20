195cfbaa97f0bd279c559bb0aa83e78070e4f524
hoya <hybriddevslim@gmail.com>
2022-11-20 20:55:44 +0900

    CashServiceImpl 수정 - 입금 금액 사용 로직 추가

--- cash-api/src/main/java/com/example/cash/service/CashServiceImpl.java
+++ cash-api/src/main/java/com/example/cash/service/CashServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.cash.dto.CashChangeView;
 import com.example.cash.dto.CashDepositView;
 import com.example.cash.repo.CashRepository;
 import com.example.error.exception.CashEmptyException;
+import com.example.error.exception.CashNotEnoughException;
 import com.example.mongo.model.Cash;
 import com.example.transaction.service.TransactionService;
 import lombok.RequiredArgsConstructor;
@@ -38,6 +39,18 @@ public class CashServiceImpl implements CashService {
                     return cash.toCashChangeView();
                 }).orElseThrow(() -> {
                     throw new CashEmptyException();
+                });
+    }
+
+    @Override
+    public void charge(Long amount) {
+        cashRepository.findFirstBy()
+                .filter(Cash::enableBalance)
+                .ifPresentOrElse(cash -> {
+                    cashRepository.save(cash.charge(amount));
+                    transactionService.charge(amount);
+                }, () -> {
+                    throw new CashNotEnoughException();
                 });
     }
 }
