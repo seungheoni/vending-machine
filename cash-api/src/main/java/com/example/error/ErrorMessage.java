782ea01cf9764350442dbbe8f0616d64a789e13a
hoya <hybriddevslim@gmail.com>
2022-11-20 20:32:16 +0900

    ErrorMessage 수정 - CASH_NOT_ENOUGH 추가

--- cash-api/src/main/java/com/example/error/ErrorMessage.java
+++ cash-api/src/main/java/com/example/error/ErrorMessage.java
@@ -3,5 +3,7 @@ package com.example.error;
 public class ErrorMessage {
     public static final String INVALID_VALIDATION = "올바르지 않은 형식입니다";
 
+    public static final String CASH_NOT_ENOUGH = "금액이 부족합니다";
+
     public static final String CASH_EMPTY = "잔액이 없습니다.";
 }
