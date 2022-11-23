c63c11e4702340ae30d4cd0f3902732752040de4
ued123 <32861341+ued123@users.noreply.github.com>
2022-11-23 23:02:43 +0900

    PurchaseService, PurchaseServiceImpl: 음료수 구매 서비스 리턴값 수정, 단위테스트 고려

--- purchase-api/src/main/java/com/example/purchase/service/PurchaseService.java
+++ purchase-api/src/main/java/com/example/purchase/service/PurchaseService.java
@@ -1,9 +1,8 @@
 package com.example.purchase.service;
 
 import com.example.purchase.dto.PurchaseDrinkPayLoad;
-import org.springframework.http.ResponseEntity;
 
 public interface PurchaseService {
 
-    ResponseEntity<String> purchaseDrink(PurchaseDrinkPayLoad purchaseDrinkPayLoad);
+    int purchaseDrink(PurchaseDrinkPayLoad purchaseDrinkPayLoad);
 }
