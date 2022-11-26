2f7167ad34476c2423316d5fb3ce738f433e6326
ued123 <32861341+ued123@users.noreply.github.com>
2022-11-26 14:08:20 +0900

    Order: 주문서 도메인 모델 생성

---
+++ purchase-api/src/main/java/com/example/mongo/model/Order.java
@@ -0,0 +1,49 @@
+package com.example.mongo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.Instant;
+
+/**
+ * 주문서
+ */
+@Document
+@Data
+@AllArgsConstructor
+public class Order {
+
+    /**
+     * 주문서 고유 번호
+     */
+    @MongoId
+    private ObjectId id;
+
+    /**
+     * 주문서 uniq 코드
+     * ex) 0020101,0020102
+     */
+    @Indexed(unique = true)
+    private String purchaseOrder;
+
+    /**
+     * 주문서 생성 날짜
+     */
+    @CreatedDate
+    private Instant createDate;
+
+    /**
+     * 음료수 제품에 대한 description
+     */
+    private String description;
+
+    /**
+     * 제품 가격
+     */
+    private long price;
+}
