433fc5df65f3de001e038d3f6989412f3911f25a
ued123 <32861341+ued123@users.noreply.github.com>
2022-11-30 21:46:03 +0900

    OrderService, OrderServiceImpl: ~~orderWrite~~ -> registerBy 메서드로 변경, 응답으로 view를 반환하지 않고 entity를 반환하도록 수정

--- purchase-api/src/main/java/com/example/order/service/OrderServiceImpl.java
+++ purchase-api/src/main/java/com/example/order/service/OrderServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.order.service;
 
 import com.example.mongo.model.Order;
 import com.example.order.dto.OrderPayLoad;
-import com.example.order.dto.OrderView;
 import com.example.order.repo.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,8 +13,8 @@ public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
 
     @Override
-    public OrderView orderWrite(OrderPayLoad orderPayLoad) {
-        Order order = orderPayLoad.toOrder();
-        return orderRepository.save(order).toOrderView();
+    public Order registerBy(OrderPayLoad orderPayLoad) {
+        Order order = Order.of(orderPayLoad);
+        return orderRepository.save(order);
     }
 }
