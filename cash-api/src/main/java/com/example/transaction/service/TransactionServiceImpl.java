2917a02593645242d0d3bcdb8c4bfe31a05e1a40
ued123 <32861341+ued123@users.noreply.github.com>
2022-12-04 18:46:16 +0900

    TransactionServiceImpl: entitymapper 도입 기존 로직 수정

--- cash-api/src/main/java/com/example/transaction/service/TransactionServiceImpl.java
+++ cash-api/src/main/java/com/example/transaction/service/TransactionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.transaction.service;
 
 import com.example.mongo.model.Transaction;
+import com.example.mongo.model.entitymapper.TransactionMapper;
 import com.example.transaction.repo.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,17 +12,19 @@ public class TransactionServiceImpl implements TransactionService {
 
     private final TransactionRepository transactionRepository;
 
+    private final TransactionMapper transactionMapper;
+
     @Override
     public Transaction deposit(Long amount) {
-        return transactionRepository.save(Transaction.ofDeposit(amount));
+        return transactionRepository.save(transactionMapper.ofDeposit(amount));
     }
 
     @Override
     public Transaction change(Long amount) {
-        return transactionRepository.save(Transaction.ofChange(amount));
+        return transactionRepository.save(transactionMapper.ofChange(amount));
     }
 
     @Override
-    public Transaction charge(Long amount) {return transactionRepository.save(Transaction.ofCharge(amount));}
+    public Transaction charge(Long amount) {return transactionRepository.save(transactionMapper.ofCharge(amount));}
 
 }
