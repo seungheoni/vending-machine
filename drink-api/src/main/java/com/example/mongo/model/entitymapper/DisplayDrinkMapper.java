180a28acebe60f51ac809a428b2c96aab8b6c49f
ued123 <32861341+ued123@users.noreply.github.com>
2022-12-08 22:51:55 +0900

    DisplayDrinkMapper: 음료 전시 entitymapper 구현, drink name 필드 추가

--- drink-api/src/main/java/com/example/mongo/model/entitymapper/DisplayDrinkMapper.java
+++ drink-api/src/main/java/com/example/mongo/model/entitymapper/DisplayDrinkMapper.java
@@ -7,18 +7,30 @@ import com.example.mongo.model.Drink;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import java.util.List;
+
 
 @Mapper(componentModel = "spring", uses = {})
 public interface DisplayDrinkMapper {
-
-    @Mapping(target = "status", source = "drink", qualifiedByName = "statusOnQuantity")
-    @Mapping(target = "drinkCode", source = "drink.code")
+    @Mapping(target = "status", source = "display.drinks", qualifiedByName = "getStatusByQuantity")
+    @Mapping(target = "drinkCode", source = "display.drinks", qualifiedByName = "getDrinkCode")
+    @Mapping(target = "name", source = "display.drinks",qualifiedByName = "getDrinkName")
     @Mapping(target = "position", source = "display.position")
-    DisplayDrinkView drinkToDisplayDrinkView(Display display , Drink drink);
+    DisplayDrinkView drinkToDisplayDrinkView(Display display);
 
-    @Named("statusOnQuantity")
-    default Status statusOnQuantity(Drink drink) {
-        return (drink.getQuantity() == 0) ? Status.SOLDOUT : Status.AVAILABLE;
+    @Named("getStatusByQuantity")
+    default Status getStatusByQuantity(List<Drink> drinks) {
+        long quantity = drinks.get(0).getQuantity();
+        return (quantity == 0) ? Status.SOLDOUT : Status.AVAILABLE;
     }
 
+    @Named("getDrinkCode")
+    default String getDrinkCode(List<Drink> drinks) {
+        return drinks.get(0).getCode();
+    }
+
+    @Named("getDrinkName")
+    default String getDrinkName(List<Drink> drinks) {
+        return drinks.get(0).getName();
+    }
 }
