038efd78d41c44a3460f804a31bb1261016c97f6
ued123 <32861341+ued123@users.noreply.github.com>
2022-12-10 22:44:07 +0900

    DisplayService, DisplayServiceImpl, DisplayServiceTest: entitymapper 도입, 기존 controller 로직을 서비스 로직으로 변경, Display -> DisplayDrinkView를 반환

--- drink-api/src/main/java/com/example/display/service/DisplayServiceImpl.java
+++ drink-api/src/main/java/com/example/display/service/DisplayServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.display.service;
 
+import com.example.display.dto.DisplayDrinkView;
 import com.example.display.repo.DisplayRepository;
 import com.example.mongo.model.Display;
+import com.example.mongo.model.entitymapper.DisplayDrinkMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +16,14 @@ public class DisplayServiceImpl implements DisplayService {
 
     private final DisplayRepository displayRepository;
 
+    private final DisplayDrinkMapper displayDrinkMapper;
+
     @Override
-    public List<Display> getDisplayDrinks() {
-        return displayRepository.findWithDrink();
+    public List<DisplayDrinkView> getDisplayDrinks() {
+
+        return  displayRepository.findWithDrink().stream()
+                .filter(Display::isDrinkRegistered)
+                .map(displayDrinkMapper::drinkToDisplayDrinkView)
+                .collect(Collectors.toList());
     }
 }
