ca922079036c42c2c41e19f2fc5beab61577646a
ued123 <32861341+ued123@users.noreply.github.com>
2022-12-16 23:28:20 +0900

    ErrorMessage: drink 용 custom error message 정의

---
+++ drink-api/src/main/java/com/example/error/ErrorMessage.java
@@ -0,0 +1,8 @@
+package com.example.error;
+
+public class ErrorMessage {
+
+    public static final String DRINK_NOT_EXIST = "음료수가 존재하지 않습니다.";
+
+    public static final String DRINK_SOLD_OUT = "해당 제품은 품절입니다.";
+}
